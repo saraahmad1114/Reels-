d9e5863d53bcefe36050c5b078a7dd422bd19552
saraahmad1114 <asara1114@gmail.com>
2018-04-13 22:35:01 -0400

    created the movie object file

---
+++ Reels/Reels/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  Reels
+//
+//  Created by Sara Ahmad on 4/13/18.
+//  Copyright © 2018 Sara Ahmad. All rights reserved.
+//
+
+import Foundation
+
+struct Movie{
+    
+    
+    //1. Define the Movie Object based on the JSON 
+}
