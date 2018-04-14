64441f3c0244b1a239bf6f5e04a9836ddddf3d84
saraahmad1114 <asara1114@gmail.com>
2018-04-14 18:25:58 -0400

    defined the movieDetail swift object

--- Reels/Reels/Movie.swift
+++ Reels/Reels/Movie.swift
@@ -19,12 +19,12 @@ struct Movie{
     init(jsonDicitonary: [String: Any]) {
         
         guard
-         let title = jsonDicitonary["Title"] as? String,
-         let year = jsonDicitonary["Year"] as? String,
-         let imdbID = jsonDicitonary["imdbID"] as? String,
-         let type = jsonDicitonary["Type"] as? String,
-         let posterUrl = jsonDicitonary["Poster"] as? String
-            else {print("JSON did not unwrap"); return}
+            let title = jsonDicitonary["Title"] as? String,
+            let year = jsonDicitonary["Year"] as? String,
+            let imdbID = jsonDicitonary["imdbID"] as? String,
+            let type = jsonDicitonary["Type"] as? String,
+            let posterUrl = jsonDicitonary["Poster"] as? String
+                else {print("JSON did not unwrap"); return}
         
         self.title = title
         self.year = year
@@ -32,7 +32,47 @@ struct Movie{
         self.type = type
         self.posterUrl = posterUrl
     }
+}
+
+struct MovieDetails {
+
+    var title: String?
+    var year: String?
+    var rating: String?
+    var releaseDate: String?
+    var genre: String?
+    var director: String?
+    var writer: String?
+    var actors: String?
+    var plot: String?
+    var posterUrl: String?
     
+    init(jsonDictionary: [String: Any]) {
+        
+        guard
+            let title = jsonDictionary["Title"] as? String,
+            let year = jsonDictionary["Year"] as? String,
+            let rating = jsonDictionary["Rated"] as? String,
+            let released = jsonDictionary["Released"] as? String,
+            let genre = jsonDictionary["Genre"] as? String,
+            let director = jsonDictionary["Director"] as? String,
+            let writer = jsonDictionary["Writer"] as? String,
+            let actors = jsonDictionary["Actors"] as? String,
+            let plot = jsonDictionary["Plot"] as? String,
+            let posterUrl = jsonDictionary["Poster"] as? String
+                else {print("movieDetails JSON did not unwrap"); return}
+        
+        self.title = title
+        self.year = year
+        self.rating = rating
+        self.releaseDate = released
+        self.genre = genre
+        self.director = director
+        self.writer = writer
+        self.actors = actors
+        self.plot = plot
+        self.posterUrl = posterUrl
+    }
 }
 
 
