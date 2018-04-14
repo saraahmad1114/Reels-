5463529255870dcdb7358b8d864ac84da67d5fd8
saraahmad1114 <asara1114@gmail.com>
2018-04-13 22:30:23 -0400

    added keys

---
+++ Reels/Reels/Secrets.swift
@@ -0,0 +1,15 @@
+//
+//  Secrets.swift
+//  Reels
+//
+//  Created by Sara Ahmad on 3/20/18.
+//  Copyright © 2018 Sara Ahmad. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets{
+    
+    var movieApiKey = "628817cf"
+    
+}
